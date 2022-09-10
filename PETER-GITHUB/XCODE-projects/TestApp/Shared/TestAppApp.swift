532618e0b82eec251d7a402c185f6cd598d9d068
Desire Namanya <100676695+DesireNamanya@users.noreply.github.com>
2022-09-10 22:42:26 +0300

    Add files via upload

---
+++ PETER-GITHUB/XCODE-projects/TestApp/Shared/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  Shared
+//
+//  Created by Desire namanya on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
